8271d4543c3f7319114ae43c65cde89ca9db6329
AyushDewangan <ayushdewangan203@gmail.com>
2023-05-05 20:18:12 +0530

    functionality working

--- src/main/java/com/example/demo/serviceImpl/TodoServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/TodoServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.serviceImpl;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import com.example.demo.service.TodoService;
 
 @Service
 public class TodoServiceImpl implements TodoService {
+
+	Logger log = LoggerFactory.getLogger(TodoServiceImpl.class);
 
 	@Autowired
 	private TodoRepository todoRepository;
@@ -33,26 +37,36 @@ public class TodoServiceImpl implements TodoService {
 
 	@Override
 	public void updateTodo(Todo todo) {
-		todoRepository.save(todo);
-		
+		try {
+			todoRepository.save(todo);
+		} catch (Exception e) {
+			log.error("exception occurred while saveTodo : ", e);
+		}
 	}
 
 	@Override
 	public void updateTodoById(int id, String todoText) {
-		Optional<Todo> todo = todoRepository.findById(id);
-		todo.get().setTodoText(todoText);
-		todoRepository.save(todo.get());
+		try {
+			Optional<Todo> todo = todoRepository.findById(id);
+			todo.get().setTodoText(todoText);
+			todoRepository.save(todo.get());
+		} catch (Exception e) {
+			log.error("exception occurred while updating text : ", e);
+		}
 	}
 
 	@Override
 	public void updateTodoStatus(int id) {
-		Optional<Todo> todo = todoRepository.findById(id);
-		if(todo.get().isCompleted()==true)
-			todo.get().setCompleted(false);
-		else
-			todo.get().setCompleted(true);
-		todoRepository.save(todo.get());
-		
+		try {
+			Optional<Todo> todo = todoRepository.findById(id);
+			if(todo.get().isCompleted() == true)
+				todo.get().setCompleted(false);
+			else
+				todo.get().setCompleted(true);
+			todoRepository.save(todo.get());
+		} catch (Exception e) {
+			log.error("exception occurred while updating status : ", e);
+		}
 	}
 
 }
